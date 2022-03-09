ea7e329fe262839231e47ff4a31a3cd72f77b5dc
HelinaKim <667585@student.inholland.nl>
2022-03-09 11:03:38 +0100

    teacherModel

--- SomerenModel/Teacher.cs
+++ SomerenModel/Teacher.cs
@@ -8,7 +8,15 @@ namespace SomerenModel
 {
     public class Teacher
     {
-        public string Name { get; set; }
-        public int Number { get; set; } // LecturerNumber, e.g. 47198
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int TeacherId { get; set; } // LecturerNumber, e.g. 47198
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+        public Teacher(string firstName, string lastName, int teacherId)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.TeacherId = teacherId;
+        }
     }
 }
