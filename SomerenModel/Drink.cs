b209b9dd26b3bc7a51fb6051cb2e50c61889b200
HelinaKim <667585@student.inholland.nl>
2022-03-18 13:20:39 +0100

    Assignment 3B

--- SomerenModel/Drink.cs
+++ SomerenModel/Drink.cs
@@ -8,16 +8,21 @@ namespace SomerenModel
 {
     public class Drink
     {
-        public int DrinkId { get; set; }
-        public string DrinkName { get; set; }
+        public int Id { get; set; } // StudentNumber, e.g. 474791
+        public string Name { get; set; }
         public int Stock { get; set; }
         public double Price { get; set; }
         public int VatId { get; set; }
         public int AmountSold { get; set; }
-
-        public Drink(string drinkName, int stock, double price, int vatId, int amountSold)
+        public Drink(int id, int vatId)
         {
-            DrinkName = drinkName;
+            Id = id;
+            VatId = vatId;
+        }
+        public Drink(int id, string name, int stock, double price, int vatId, int amountSold)
+        {
+            Id = id;
+            Name = name;
             Stock = stock;
             Price = price;
             VatId = vatId;
---
+++ SomerenModel/OrderLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public  class OrderLine
+    {
+        public int Amount { get; set; }
+        public Drink Drink { get; set; }
+
+        public OrderLine(int amount, Drink drink)
+        {
+            this.Amount = amount;
+            this.Drink = drink;
+        }
+    }
+}
