e92328786b3320fbdf54de69cb48e8eaf586974f
Alandehaas <564259@student.inholland.nl>
2022-03-26 18:00:49 +0100

    new login form

---
+++ SomerenUI/SomerenUILogin.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SomerenUI
+{
+    partial class SomerenUILogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameTextbox = new System.Windows.Forms.TextBox();
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.forgotPasswordLabel = new System.Windows.Forms.Label();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(60, 39);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(55, 13);
+            this.usernameLabel.TabIndex = 0;
+            this.usernameLabel.Text = "Username";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(60, 78);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(53, 13);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Password";
+            // 
+            // usernameTextbox
+            // 
+            this.usernameTextbox.Location = new System.Drawing.Point(63, 55);
+            this.usernameTextbox.Name = "usernameTextbox";
+            this.usernameTextbox.Size = new System.Drawing.Size(206, 20);
+            this.usernameTextbox.TabIndex = 2;
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.Location = new System.Drawing.Point(63, 94);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.PasswordChar = '*';
+            this.passwordTextbox.Size = new System.Drawing.Size(206, 20);
+            this.passwordTextbox.TabIndex = 3;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(63, 158);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(206, 30);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // forgotPasswordLabel
+            // 
+            this.forgotPasswordLabel.AutoSize = true;
+            this.forgotPasswordLabel.Location = new System.Drawing.Point(178, 131);
+            this.forgotPasswordLabel.Name = "forgotPasswordLabel";
+            this.forgotPasswordLabel.Size = new System.Drawing.Size(91, 13);
+            this.forgotPasswordLabel.TabIndex = 5;
+            this.forgotPasswordLabel.Text = "Forgot password?";
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(63, 194);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(206, 32);
+            this.registerButton.TabIndex = 6;
+            this.registerButton.Text = "Register new user";
+            this.registerButton.UseVisualStyleBackColor = true;
+            // 
+            // SomerenUILogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 264);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.forgotPasswordLabel);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextbox);
+            this.Controls.Add(this.usernameTextbox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Name = "SomerenUILogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox usernameTextbox;
+        private System.Windows.Forms.TextBox passwordTextbox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label forgotPasswordLabel;
+        private System.Windows.Forms.Button registerButton;
+    }
+}
---
+++ SomerenUI/SomerenUILogin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SomerenUI
+{
+    public partial class SomerenUILogin : Form
+    {
+        const string userName = "Alan";
+        const string password = "123456abc";
+        public SomerenUILogin()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(usernameTextbox.Text) || !String.IsNullOrEmpty(passwordTextbox.Text))
+            {
+                if (usernameTextbox.Text != userName || passwordTextbox.Text != password)
+                {
+                    MessageBox.Show("Please enter correct Username and Password");
+                }
+                else
+                {
+                    this.Hide();
+                    SomerenUI form = new SomerenUI();
+                    form.ShowDialog();
+                }
+            }
+            else MessageBox.Show("Please enter correct Username and Password");
+        }
+    }
+}
